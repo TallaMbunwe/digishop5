383b51d99edc71bad4b265599531f66ac40f8b79
Eric TALLA <eric.talla@digiplusconsulting.com>
2019-07-22 19:38:03 +0100

    Initial Commit

---
+++ src/main/java/net/ds5/digishop5/Digishop5Application.java
@@ -0,0 +1,13 @@
+package net.ds5.digishop5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Digishop5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Digishop5Application.class, args);
+	}
+
+}
