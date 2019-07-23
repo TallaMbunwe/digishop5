56bf1232c416849b07184457137c64d95c77556a
Eric TALLA <eric.talla@digiplusconsulting.com>
2019-07-23 18:34:00 +0100

    update commit login

--- src/main/java/net/ds5/config/SecurityConfig.java
+++ src/main/java/net/ds5/config/SecurityConfig.java
@@ -1,32 +1,21 @@
 package net.ds5.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Bean
 	@Override
-	public UserDetailsService userDetailsServiceBean() throws Exception {
-
-		// add users in List
-		List<UserDetails> users = new ArrayList<UserDetails>();
-		
-		users.add(User.withDefaultPasswordEncoder()
-				.username("root")
-				.password("admin").roles("USER").build());
-
-		return new InMemoryUserDetailsManager(users);
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.authorizeRequests().antMatchers("/login").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin().loginPage("/login").permitAll();
 	}
 }
