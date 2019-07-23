81b96ff3e41e1e60328730d69f153d66924bdc02
Eric TALLA <eric.talla@digiplusconsulting.com>
2019-07-23 22:35:31 +0100

    update commit

--- src/main/java/net/ds5/config/SecurityConfig.java
+++ src/main/java/net/ds5/config/SecurityConfig.java
@@ -1,30 +1,32 @@
 package net.ds5.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Bean
 	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/resources/**");
-	}
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/login").permitAll()
-			.anyRequest().authenticated()
-			.and()
-			.formLogin()
-			.loginPage("/login")
-			.usernameParameter("userId")
-			.passwordParameter("pwd")
-			.permitAll();
+	public UserDetailsService userDetailsServiceBean() throws Exception {
+
+		// add users in List
+		List<UserDetails> users = new ArrayList<UserDetails>();
 		
-		http.csrf().disable();
+		users.add(User.withDefaultPasswordEncoder()
+				.username("root")
+				.password("admin").roles("USER").build());
+
+		return new InMemoryUserDetailsManager(users);
 	}
 }
